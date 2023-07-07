0b2a79406d83d624110602a5b9ab2b3bc0bb8c2c
Hakan ÖZER <hakanozer02@gmail.com>
2023-07-07 15:10:47 +0300

    Session Manager

--- Hazelcast/Application/src/main/java/com/works/restcontroller/BookRestController.java
+++ Hazelcast/Application/src/main/java/com/works/restcontroller/BookRestController.java
@@ -2,6 +2,7 @@ package com.works.restcontroller;
 
 import com.works.entities.Book;
 import com.works.entities.BookAuthorJoin;
+import com.works.models.Admin;
 import com.works.services.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,22 +10,50 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class BookRestController {
 
     final BookService bookService;
+    final HttpServletRequest request;
 
     @PostMapping("/bookSave")
-    public Book bookSave(@RequestBody Book book){
+    public Object bookSave(@RequestBody Book book){
+        boolean status = request.getSession().getAttribute("admin") == null;
+        if ( status ) {
+            Map<String, Object> hm = new LinkedHashMap<>();
+            hm.put("status", false);
+            hm.put("message", "Please Login");
+            return hm;
+        }
         return bookService.save(book);
     }
 
     @GetMapping("/bookList")
-    public List<BookAuthorJoin> bookList() {
+    public Object bookList() {
+        boolean status = request.getSession().getAttribute("admin") == null;
+        if ( status ) {
+            Map<String, Object> hm = new LinkedHashMap<>();
+            hm.put("status", false);
+            hm.put("message", "Please Login");
+            return hm;
+        }
         return bookService.books();
+    }
+
+
+    @PostMapping("/login")
+    public boolean login(@RequestBody Admin admin) {
+        if (  admin.getUsername().equals("ali01") && admin.getPassword().equals("12345")) {
+            request.getSession().setAttribute("admin", admin.getUsername());
+            return true;
+        }
+        return false;
     }
 
 
